5b7826c2b0f0a7a5df3a588df0d30e7470eb45c9
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-02-26 11:54:46 +0100

    svg z2

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Point point = new Point();
+        point.x = 5.6F;
+        point.y = 2.4F;
+
+        Point p2 = new Point();
+        p2.x = 150F;
+        p2.y = 213.7F;
+        System.out.println(p2.toSvg());
+
     }
 }
