5688248e2f4757c8b5b3ae576a0c27ed47f9dd14
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-03-12 10:33:42 +0100

    svg2 z8

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Point[] points = new Point[5];
         points[0] = new Point(2.3F, 5.6F);
         points[1] = new Point(10.5F, 12.4F);
@@ -23,5 +25,6 @@ public class Main {
         //scene.addPolygon(polygon3);
         //svg.addPolygon(polygon3);
         System.out.println(scene.toSvg());
+        scene.save("out.svg");
     }
 }
--- svg/src/SvgScene.java
+++ svg/src/SvgScene.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Locale;
 
 public class SvgScene {
@@ -13,12 +15,19 @@ public class SvgScene {
 
     public String toSvg(){
         StringBuilder polygonsString = new StringBuilder();
+        polygonsString.append("\n");
         for (var polygon: polygons){
             if (polygon != null) {
+                polygonsString.append("\t");
                 polygonsString.append(polygon.toSvg())
                         .append("\n");
             }
         }
         return String.format(Locale.ENGLISH, "<svg height=\"260\" width=\"500\" xmlns=\"http://www.w3.org/2000/svg\">%s</svg>", polygonsString);
+    }
+    public void save(String path) throws IOException {
+        FileWriter writer = new FileWriter(path);
+        writer.write(toSvg());
+        writer.close();
     }
 }
