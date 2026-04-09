b3a2f5da4d5f7dac065ddd3be193887d024b5995
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-04-09 11:22:34 +0200

    gen2 z2

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        //Person person = Person.fromCsvLine("Marek Kowalski,15.05.1899,,,");
-        //System.out.println(person);
         try {
             List<Person> people = Person.fromCsv("family.csv");
             System.out.println(people);
