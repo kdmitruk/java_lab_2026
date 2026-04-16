d5548477af6dcf14a13ac4298103cd4dbcd5b94c
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-04-16 11:52:46 +0200

    gen2 z7

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -6,11 +6,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
         try {
-            List<Person> people = Person.fromCsv("family.csv");
+            //List<Person> people = Person.fromCsv("family.csv");
+            List<Person> people = Person.fromBinaryFile("data.bin");
             for(Person person: people)
                 System.out.println(person);
+            //Person.toBinaryFile("data.bin", people);
         } catch (IOException e) {
             System.err.println("Blad dostepu do pliku");
-        }
+            throw new RuntimeException(e);
+       } catch (ClassNotFoundException e) {
+          throw new RuntimeException(e);
+      }
     }
 }
