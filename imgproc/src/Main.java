142a2c8924e047544e1e02a444d4148fb8a9bda7
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-05-28 10:29:58 +0200

    imgproc z1

--- imgproc/src/Main.java
+++ imgproc/src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ImageProcessor processor = new ImageProcessor();
+        processor.load("lena.png");
+        processor.save("lena.jpg");
     }
 }
