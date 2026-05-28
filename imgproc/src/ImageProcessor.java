3f3904b26d8fcdfe2b1699c1f1377676be625f85
Krzysztof Dmitruk <kdmitruk@gmail.com>
2026-05-28 10:50:18 +0200

    imgproc z2

--- imgproc/src/ImageProcessor.java
+++ imgproc/src/ImageProcessor.java
@@ -3,6 +3,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import static java.lang.Math.clamp;
+
 public class ImageProcessor {
     private BufferedImage img;
 
@@ -14,4 +16,21 @@ public class ImageProcessor {
         File file = new File(path);
         ImageIO.write(this.img, "jpg", file);
     }
+    public void addBrightness(int amount) {
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                int color = img.getRGB(x, y);
+                int blue =   color & 0x0000FF;
+                int green = (color & 0x00FF00) >> 8;
+                int red =   (color & 0xFF0000) >> 16;
+
+                blue = Math.clamp((blue + amount), 0, 255);
+                green = Math.clamp((green + amount), 0, 255);
+                red = Math.clamp((red + amount), 0, 255);
+
+                img.setRGB(x, y, blue | (green << 8) | (red << 16));
+            }
+        }
+    }
+
 }
--- imgproc/src/Main.java
+++ imgproc/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ImageProcessor processor = new ImageProcessor();
         processor.load("lena.png");
+        processor.addBrightness(-100);
         processor.save("lena.jpg");
     }
 }
